e04eb4e44a254d11de7e3c74cefe7085eeeaaa6d
Scott Cooper <scttcper@gmail.com>
2020-03-31 21:40:38 -0700

    chore: upgrade dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,9 @@ module.exports = {
     'object-curly-spacing': ['error', 'always'],
     '@typescript-eslint/indent': ['error', 2, { SwitchCase: 1 }],
     'capitalized-comments': 0,
+    '@typescript-eslint/no-unsafe-call': 0,
+    '@typescript-eslint/no-unsafe-member-access': 0,
+    '@typescript-eslint/prefer-readonly-parameter-types': 0,
     'comma-dangle': ['error', 'always-multiline'],
   },
 };
