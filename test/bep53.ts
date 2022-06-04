b31d07b213f1d4200b1287e7740c7a95053bb0a7
Scott Cooper <scttcper@users.noreply.github.com>
2022-06-04 15:00:12 -0700

    fix: upgrade dependencies (#91)

--- test/bep53.ts
+++ test/bep53.ts
@@ -1,51 +1,51 @@
-import test from 'ava';
+import { expect, test } from 'vitest';
 
 import { composeRange, parseRange } from '../src/bep53.js';
 
-test('parse: a number', t => {
+test('parse: a number', () => {
   const range = parseRange(['1']);
 
-  t.deepEqual(range, [1]);
+  expect(range).toEqual([1]);
 });
 
-test('parse: one range', t => {
+test('parse: one range', () => {
   const range = parseRange(['1-3']);
 
-  t.deepEqual(range, [1, 2, 3]);
+  expect(range).toEqual([1, 2, 3]);
 });
 
-test('parse: multiple ranges', t => {
+test('parse: multiple ranges', () => {
   const range = parseRange(['1-3', '11-13']);
 
-  t.deepEqual(range, [1, 2, 3, 11, 12, 13]);
+  expect(range).toEqual([1, 2, 3, 11, 12, 13]);
 });
 
-test('parse: multiple ranges between a number', t => {
+test('parse: multiple ranges between a number', () => {
   const range = parseRange(['1-3', '6', '11-13']);
 
-  t.deepEqual(range, [1, 2, 3, 6, 11, 12, 13]);
+  expect(range).toEqual([1, 2, 3, 6, 11, 12, 13]);
 });
 
-test('compose: a number', t => {
+test('compose: a number', () => {
   const range = composeRange([1]);
 
-  t.deepEqual(range, ['1']);
+  expect(range).toEqual(['1']);
 });
 
-test('compose: one range', t => {
+test('compose: one range', () => {
   const range = composeRange([1, 2, 3]);
 
-  t.deepEqual(range, ['1-3']);
+  expect(range).toEqual(['1-3']);
 });
 
-test('compose: multiple ranges', t => {
+test('compose: multiple ranges', () => {
   const range = composeRange([1, 2, 3, 11, 12, 13]);
 
-  t.deepEqual(range, ['1-3', '11-13']);
+  expect(range).toEqual(['1-3', '11-13']);
 });
 
-test('compose: multiple ranges between a number', t => {
+test('compose: multiple ranges between a number', () => {
   const range = composeRange([1, 2, 3, 6, 11, 12, 13]);
 
-  t.deepEqual(range, ['1-3', '6', '11-13']);
+  expect(range).toEqual(['1-3', '6', '11-13']);
 });
--- test/decode.spec.ts
+++ test/decode.spec.ts
@@ -1,4 +1,4 @@
-import test from 'ava';
+import { expect, test } from 'vitest';
 
 import { MagnetData, magnetDecode } from '../src/index.js';
 
@@ -7,7 +7,7 @@ const leavesOfGrass =
 
 const empty = { announce: [], peerAddresses: [], urlList: [] };
 
-test('should decode', t => {
+test('should decode', () => {
   const result: MagnetData = {
     announce: [
       'udp://tracker.example1.com:1337',
@@ -31,93 +31,93 @@ test('should decode', t => {
     urlList: [],
     xt: 'urn:btih:d2474e86c95b19b8bcfdb92bc12c9d44667cfa36',
   };
-  t.deepEqual(magnetDecode(leavesOfGrass), result);
+  expect(magnetDecode(leavesOfGrass)).toEqual(result);
 });
 
-test('should decode empty magnet URIs return empty object', t => {
+test('should decode empty magnet URIs return empty object', () => {
   const empty1 = '';
   const empty2 = 'magnet:';
   const empty3 = 'magnet:?';
 
-  t.deepEqual(magnetDecode(empty1), empty);
-  t.deepEqual(magnetDecode(empty2), empty);
-  t.deepEqual(magnetDecode(empty3), empty);
+  expect(magnetDecode(empty1)).toEqual(empty);
+  expect(magnetDecode(empty2)).toEqual(empty);
+  expect(magnetDecode(empty3)).toEqual(empty);
 });
 
-test('empty string as keys is okay', t => {
+test('empty string as keys is okay', () => {
   const uri = 'magnet:?a=&b=&c=';
   const blank = { a: '', b: '', c: '' };
-  t.deepEqual(magnetDecode(uri), { ...blank, ...empty });
+  expect(magnetDecode(uri)).toEqual({ ...blank, ...empty });
 });
 
-test('should decode invalid magnet URIs return empty object', t => {
+test('should decode invalid magnet URIs return empty object', () => {
   const invalid1 = 'magnet:?xt=urn:btih:===';
   const invalid2 = 'magnet:?xt';
   const invalid3 = 'magnet:?xt=?dn=';
 
-  t.deepEqual(magnetDecode(invalid1), empty);
-  t.deepEqual(magnetDecode(invalid2), empty);
-  t.deepEqual(magnetDecode(invalid3), empty);
+  expect(magnetDecode(invalid1)).toEqual(empty);
+  expect(magnetDecode(invalid2)).toEqual(empty);
+  expect(magnetDecode(invalid3)).toEqual(empty);
 });
 
-test('should decode invalid magnet URIs return only valid keys (ignoring invalid ones)', t => {
+test('should decode invalid magnet URIs return only valid keys (ignoring invalid ones)', () => {
   const invalid1 = 'magnet:?a=a&===';
   const invalid2 = 'magnet:?a==&b=b';
   const invalid3 = 'magnet:?a=b=&c=c&d===';
 
-  t.deepEqual(magnetDecode(invalid1), { a: 'a', ...empty });
-  t.deepEqual(magnetDecode(invalid2), { b: 'b', ...empty });
-  t.deepEqual(magnetDecode(invalid3), { c: 'c', ...empty });
+  expect(magnetDecode(invalid1)).toEqual({ a: 'a', ...empty });
+  expect(magnetDecode(invalid2)).toEqual({ b: 'b', ...empty });
+  expect(magnetDecode(invalid3)).toEqual({ c: 'c', ...empty });
 });
 
-test('should decode extracts 40-char hex BitTorrent info_hash', t => {
+test('should decode extracts 40-char hex BitTorrent info_hash', () => {
   const result = magnetDecode('magnet:?xt=urn:btih:aad050ee1bb22e196939547b134535824dabf0ce');
-  t.is(result.infoHash, 'aad050ee1bb22e196939547b134535824dabf0ce');
+  expect(result.infoHash).toBe('aad050ee1bb22e196939547b134535824dabf0ce');
 });
 
-test('should decode extracts 32-char base32 BitTorrent info_hash', t => {
+test('should decode extracts 32-char base32 BitTorrent info_hash', () => {
   const result = magnetDecode('magnet:?xt=urn:btih:64DZYZWMUAVLIWJUXGDIK4QGAAIN7SL6');
-  t.is(result.infoHash, 'f7079c66cca02ab45934b9868572060010dfc97e');
+  expect(result.infoHash).toBe('f7079c66cca02ab45934b9868572060010dfc97e');
 });
 
-test('should decode extracts keywords', t => {
+test('should decode extracts keywords', () => {
   const result = magnetDecode(
     'magnet:?xt=urn:btih:64DZYZWMUAVLIWJUXGDIK4QGAAIN7SL6&kt=joe+blow+mp3',
   );
-  t.deepEqual(result.keywords, ['joe', 'blow', 'mp3']);
+  expect(result.keywords).toEqual(['joe', 'blow', 'mp3']);
 });
 
-test('should decode complicated magnet uri (multiple xt params, and as, xs)', t => {
+test('should decode complicated magnet uri (multiple xt params, and as, xs)', () => {
   const result = magnetDecode(
     'magnet:?xt=urn:ed2k:354B15E68FB8F36D7CD88FF94116CDC1&xt=urn:tree:tiger:7N5OAMRNGMSSEUE3ORHOKWN4WWIQ5X4EBOOTLJY&xt=urn:btih:QHQXPYWMACKDWKP47RRVIV7VOURXFE5Q&xl=10826029&dn=mediawiki-1.15.1.tar.gz&tr=udp%3A%2F%2Ftracker.example4.com%3A80%2Fannounce&as=http%3A%2F%2Fdownload.wikimedia.org%2Fmediawiki%2F1.15%2Fmediawiki-1.15.1.tar.gz&xs=http%3A%2F%2Fcache.example.org%2FXRX2PEFXOOEJFRVUCX6HMZMKS5TWG4K5&xs=dchub://example.org',
   );
-  t.is(result.infoHash, '81e177e2cc00943b29fcfc635457f575237293b0');
-  t.deepEqual(result.xt, [
+  expect(result.infoHash).toBe('81e177e2cc00943b29fcfc635457f575237293b0');
+  expect(result.xt).toEqual([
     'urn:ed2k:354B15E68FB8F36D7CD88FF94116CDC1',
     'urn:tree:tiger:7N5OAMRNGMSSEUE3ORHOKWN4WWIQ5X4EBOOTLJY',
     'urn:btih:QHQXPYWMACKDWKP47RRVIV7VOURXFE5Q',
   ]);
-  t.is(result.xl, '10826029');
-  t.is(result.dn, 'mediawiki-1.15.1.tar.gz');
+  expect(result.xl).toBe('10826029');
+  expect(result.dn).toBe('mediawiki-1.15.1.tar.gz');
 
   const announce = 'udp://tracker.example4.com:80/announce';
-  t.is(result.tr, announce);
-  t.deepEqual(result.announce, [announce]);
-  t.is(result.as, 'http://download.wikimedia.org/mediawiki/1.15/mediawiki-1.15.1.tar.gz');
-  t.deepEqual(result.urlList, [
+  expect(result.tr).toBe(announce);
+  expect(result.announce).toEqual([announce]);
+  expect(result.as).toBe('http://download.wikimedia.org/mediawiki/1.15/mediawiki-1.15.1.tar.gz');
+  expect(result.urlList).toEqual([
     'http://download.wikimedia.org/mediawiki/1.15/mediawiki-1.15.1.tar.gz',
   ]);
-  t.deepEqual(result.xs, [
+  expect(result.xs).toEqual([
     'http://cache.example.org/XRX2PEFXOOEJFRVUCX6HMZMKS5TWG4K5',
     'dchub://example.org',
   ]);
 });
 
-test('should decode multiple as, ws params', t => {
+test('should decode multiple as, ws params', () => {
   const result = magnetDecode(
     'magnet:?xt=urn:ed2k:354B15E68FB8F36D7CD88FF94116CDC1&as=http%3A%2F%2Fdownload.wikimedia.org%2Fmediawiki%2F1.15%2Fmediawiki-1.15.1.tar.gz&as=http%3A%2F%2Fdownload.wikimedia.org%2Fmediawiki%2F1.15%2Fmediawiki-1.15.1.tar.gz1&ws=http%3A%2F%2Fdownload.wikimedia.org%2Fmediawiki%2F1.15%2Fmediawiki-1.15.1.tar.gz2&ws=http%3A%2F%2Fdownload.wikimedia.org%2Fmediawiki%2F1.15%2Fmediawiki-1.15.1.tar.gz3',
   );
-  t.deepEqual(result.urlList, [
+  expect(result.urlList).toEqual([
     'http://download.wikimedia.org/mediawiki/1.15/mediawiki-1.15.1.tar.gz',
     'http://download.wikimedia.org/mediawiki/1.15/mediawiki-1.15.1.tar.gz1',
     'http://download.wikimedia.org/mediawiki/1.15/mediawiki-1.15.1.tar.gz2',
@@ -125,7 +125,7 @@ test('should decode multiple as, ws params', t => {
   ]);
 });
 
-test('should decode dedupe repeated trackers', t => {
+test('should decode dedupe repeated trackers', () => {
   const result = magnetDecode(
     'magnet:?xt=urn:ed2k:354B15E68FB8F36D7CD88FF94116CDC1&tr=udp%3A%2F%2Ftracker.example4.com%3A80&tr=udp%3A%2F%2Ftracker.example4.com%3A80&tr=udp%3A%2F%2Ftracker.example5.com%3A80&tr=udp%3A%2F%2Ftracker.example3.com%3A6969&tr=udp%3A%2F%2Ftracker.example2.com%3A80&tr=udp%3A%2F%2Ftracker.example1.com%3A1337',
   );
@@ -136,78 +136,80 @@ test('should decode dedupe repeated trackers', t => {
     'udp://tracker.example4.com:80',
     'udp://tracker.example5.com:80',
   ];
-  t.deepEqual(result.announce, announce);
+  expect(result.announce).toEqual(announce);
 });
 
-test('Cast file index (ix) to a number', t => {
+test('Cast file index (ix) to a number', () => {
   const result = magnetDecode(`${leavesOfGrass}&ix=1`);
-  t.is(typeof result.ix, 'number');
-  t.is(result.ix, 1);
+  expect(typeof result.ix).toBe('number');
+  expect(result.ix).toBe(1);
 });
 
-test('should decode bittorrent v2 magnet links', t => {
+test('should decode bittorrent v2 magnet links', () => {
   const result = magnetDecode(
     'magnet:?xt=urn:btmh:1220caf1e1c30e81cb361b9ee167c4aa64228a7fa4fa9f6105232b28ad099f3a302e&dn=bittorrent-v2-test',
   );
-  t.is(result.xt, 'urn:btmh:1220caf1e1c30e81cb361b9ee167c4aa64228a7fa4fa9f6105232b28ad099f3a302e');
+  expect(result.xt).toBe(
+    'urn:btmh:1220caf1e1c30e81cb361b9ee167c4aa64228a7fa4fa9f6105232b28ad099f3a302e',
+  );
 });
 
-test('should decode hybrid bittorent v2 magnet links', t => {
+test('should decode hybrid bittorent v2 magnet links', () => {
   // https://blog.libtorrent.org/2020/09/bittorrent-v2/
   const result = magnetDecode(
     'magnet:?xt=urn:btih:631a31dd0a46257d5078c0dee4e66e26f73e42ac&xt=urn:btmh:1220d8dd32ac93357c368556af3ac1d95c9d76bd0dff6fa9833ecdac3d53134efabb&dn=bittorrent-v1-v2-hybrid-test',
   );
-  t.deepEqual(result.xt, [
+  expect(result.xt).toEqual([
     'urn:btih:631a31dd0a46257d5078c0dee4e66e26f73e42ac',
     'urn:btmh:1220d8dd32ac93357c368556af3ac1d95c9d76bd0dff6fa9833ecdac3d53134efabb',
   ]);
-  t.is(result.dn, 'bittorrent-v1-v2-hybrid-test');
+  expect(result.dn).toBe('bittorrent-v1-v2-hybrid-test');
 });
 
-test('decode: Extracts public key from xs', t => {
+test('decode: Extracts public key from xs', () => {
   const key = '9a36edf0988ddc1a0fc02d4e8652cce87a71aaac71fce936e650a597c0fb72e0';
   const result = magnetDecode(`magnet:?xs=urn:btpk:${key}`);
-  t.is(result.publicKey, key);
-  t.deepEqual(result.publicKeyBuffer, Buffer.from(key, 'hex'));
+  expect(result.publicKey).toBe(key);
+  expect(result.publicKeyBuffer).toEqual(Buffer.from(key, 'hex'));
 });
 
 // Select specific file indices for download (BEP53) http://www.bittorrent.org/beps/bep_0053.html
-test('decode: select-only', t => {
+test('decode: select-only', () => {
   const result = magnetDecode('magnet:?xt=urn:btih:64DZYZWMUAVLIWJUXGDIK4QGAAIN7SL6&so=0,2,4,6-8');
-  t.deepEqual(result.so, [0, 2, 4, 6, 7, 8]);
+  expect(result.so).toEqual([0, 2, 4, 6, 7, 8]);
 });
 
 // Peer address expressed as hostname:port (BEP09) http://bittorrent.org/beps/bep_0009.html
-test('decode: peer-address single value', t => {
+test('decode: peer-address single value', () => {
   const result = magnetDecode(
     'magnet:?xt=urn:btih:64DZYZWMUAVLIWJUXGDIK4QGAAIN7SL6&x.pe=123.213.32.10:47450',
   );
   const peerAddresses = ['123.213.32.10:47450'];
-  t.deepEqual(result['x.pe'], peerAddresses[0]);
-  t.deepEqual(result.peerAddresses, peerAddresses);
+  expect(result['x.pe'], peerAddresses[0]);
+  expect(result.peerAddresses).toEqual(peerAddresses);
 });
 
-test('decode: peer-address multiple values', t => {
+test('decode: peer-address multiple values', () => {
   const result = magnetDecode(
     'magnet:?xt=urn:btih:64DZYZWMUAVLIWJUXGDIK4QGAAIN7SL6&x.pe=123.213.32.10:47450&x.pe=[2001:db8::2]:55013',
   );
   const peerAddresses = ['123.213.32.10:47450', '[2001:db8::2]:55013'];
-  t.deepEqual(result['x.pe'], peerAddresses);
-  t.deepEqual(result.peerAddresses, peerAddresses);
+  expect(result['x.pe']).toEqual(peerAddresses);
+  expect(result.peerAddresses).toEqual(peerAddresses);
 });
 
-test('decode: peer-address remove duplicates', t => {
+test('decode: peer-address remove duplicates', () => {
   const result = magnetDecode(
     'magnet:?xt=urn:btih:64DZYZWMUAVLIWJUXGDIK4QGAAIN7SL6&x.pe=123.213.32.10:47450&x.pe=[2001:db8::2]:55013&x.pe=123.213.32.10:47450',
   );
 
   // raw value is *not* deduped
-  t.deepEqual(result['x.pe'], [
+  expect(result['x.pe']).toEqual([
     '123.213.32.10:47450',
     '[2001:db8::2]:55013',
     '123.213.32.10:47450',
   ]);
 
   // friendly value is deduped
-  t.deepEqual(result.peerAddresses, ['123.213.32.10:47450', '[2001:db8::2]:55013']);
+  expect(result.peerAddresses).toEqual(['123.213.32.10:47450', '[2001:db8::2]:55013']);
 });
