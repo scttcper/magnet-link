f8ffde1a4f42cf94f8fbfa4cbcbfebd5ac134bc5
Scott Cooper <scttcper@gmail.com>
2023-12-26 17:51:12 -0800

    fix: bump dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    singleThread: true,
     coverage: {
       reporter: ['text', 'json', 'html'],
       provider: 'v8',
